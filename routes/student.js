b356a4a6a7678f71386b72415770f4d808af8545
Shubham Sharma <shubhamsharma21505@gmail.com>
2024-09-20 19:44:35 +0530

    done

---
+++ routes/student.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const studentcontroller = require('../controllers/studentcontrollers')
+router.use(express.urlencoded({extended:false}));
+router.get('/',(req,res)=>{
+    res.render('home');
+})
+router.post('/add/student',(req,res)=>{
+    studentcontroller.addStudent(req,res)
+})
+module.exports = router 
